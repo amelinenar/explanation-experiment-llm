1cf696ea76ed3a29994f695e39402cfe75fc5028
Ameline Nguenang <amelinenar@gmail.com>
2026-03-16 15:26:14 +0100

    modified main

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ from utils.utils import create_directory
 from utils.utils import read_dataset
 from utils.utils import read_all_dataset
 from utils.utils import generate_results
+from utils.utils import filter_automl_logs
 
 from utils.constant import TASK
 from utils.constant import LLMs, LLMs_judge
 from utils.constant import dataset_names_for_task
-from utils.constant import SUMMARIZATION_PROMPT,JUDGING_PROMPT
+from utils.constant import SUMMARIZATION_PROMPT,JUDGING_PROMPT, HIERARCHICAL_PROMPT
 
 
 from prompt import explain_process,judging_explanation
 from dotenv import load_dotenv
 from result_script import write_csv, result_script
+# from pipeline.stage0_phase_segmentation import explain_process_T
+# from pipeline.stage1_dataprofiling import judging_explanation_T
+from pipeline.Hierarchical_pipeline import phase_segmentation, micro_summarization,extract_pure_json,macro_summarization,verification,revised_summary,regex_filtering,fact_aggregation
+from pipeline.Hierarchical_pipeline import fact_extraction
 
 from concurrent.futures import ThreadPoolExecutor
 import gc
 
 
-def create_fit_classifier(task_name,X_train,y_train, X_test,y_test,target_column,logs_path):
+
+
+def create_fit_classifier(task_name,X_train,y_train,target_column,logs_path,date_column):
     if task_name == 'REGRESSION':
           
         from alpha_automl import AutoMLRegressor
@@ -42,7 +49,7 @@ def create_fit_classifier(task_name,X_train,y_train, X_test,y_test,target_column
     elif task_name.lower() == 'time_series_forecast':
         
         from alpha_automl import AutoMLTimeSeries
-        automl = AutoMLTimeSeries(time_bound=1, date_column='Date', target_column=target_column, txt_file = logs_path)
+        automl = AutoMLTimeSeries(time_bound=1, date_column=date_column, target_column=target_column, txt_file = logs_path)
         automl.fit(X_train, y_train)
 
     elif task_name.lower() == 'semisupervised':
@@ -78,14 +85,21 @@ def iterate_loop():
     jobs = []
     for task in TASK:
         for dataset_name, sum_llm, sum_prompt in product(dataset_names_for_task[task], LLMs, SUMMARIZATION_PROMPT):
-            output_directory = root_dir + '/results/' + task + '/' + dataset_name + '/' + sum_llm + '/' + sum_prompt + '/'
+            output_directory = root_dir + '/results_H_new/' + task + '/' + dataset_name + '/' + sum_llm + '/'
             create_directory(output_directory)
-            summary_dir =  os.path.join(output_directory , 'summary_result.txt')
-            logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'full_log_MainProcess.txt')
+            summary_dir =  os.path.join(output_directory , 'summary_test.txt')
+            if(task == "CLASSIFICATION") | (task == "REGRESSION"):
+                logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'filter_logs.txt')
+            else:
+                logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'full_log_MainProcess.txt')
+                
+            # logs_path = "/home/nguenang/Master_thesis/experiment_setup/results/AUTOSKLN_LOGS/CLASSIFICATION/logs_cls.txt"
 
             jobs.append((logs_path, summary_dir, sum_prompt, output_directory,sum_llm,task,dataset_name))
 
     return jobs
+
+
             
     
 
@@ -116,14 +130,14 @@ if __name__ == "__main__":
                 
                 x_train = datasets_dict[dataset_name][0]
                 y_train = datasets_dict[dataset_name][1]
-                x_test = datasets_dict[dataset_name][2]
-                y_test = datasets_dict[dataset_name][3] 
+                # x_test = datasets_dict[dataset_name][2]
+                # y_test = datasets_dict[dataset_name][3] 
                 target_column = datasets_dict[dataset_name][4]
 
 
                 print('-----------------START FITTING--------------')
                 
-                create_fit_classifier(task_name,x_train,y_train,x_test,y_test,target_column)
+                create_fit_classifier(task_name,x_train,y_train,target_column)
                 
                 print('--------------------DONE--------------------')
                 print(" ")
@@ -166,6 +180,7 @@ if __name__ == "__main__":
         for task_name in TASK:
             print(" ")
             print('task name: ', task_name)
+            print(f"DATASET : {dataset_names_for_task[task_name]}")
             datasets_dict = read_all_dataset(root_dir, task_name)
             tmp_output_directory = root_dir + '/results/' + task_name + '/' 
             print(f"DATASET : {dataset_names_for_task[task_name]}")
@@ -174,61 +189,288 @@ if __name__ == "__main__":
                 print('\tdataset name: ', dataset_name)
                 print(" ")
                 
+                # datasets_dict = read_all_dataset(root_dir, task_name)
+                
                 x_train = datasets_dict[dataset_name][0]
                 y_train = datasets_dict[dataset_name][1]
-                x_test = datasets_dict[dataset_name][2]
-                y_test = datasets_dict[dataset_name][3] 
-                target_column = datasets_dict[dataset_name][4]
+                # x_test = datasets_dict[dataset_name][2]
+                # y_test = datasets_dict[dataset_name][3] 
+                target_column = datasets_dict[dataset_name][2]
+                date_column = datasets_dict[dataset_name][3]
                 
                 output_directory = tmp_output_directory + dataset_name + '/'
 
                 print('-----------------START FITTING--------------')
                 
-                create_fit_classifier(task_name,x_train,y_train,x_test,y_test,target_column, output_directory)
+                create_fit_classifier(task_name,x_train,y_train,target_column, output_directory, date_column)
                 
                 print('--------------------DONE--------------------')
                 print(" ")
+               
+               
                 
+              
+              
+                logs_path = os.path.join(output_directory, 'full_log_MainProcess.txt')
+                
+                ### Filter the logs
+                
+                if(task_name =="CLASSIFICATION") | (task_name =="REGRESSION"):
+                    print("------------FILTERING THE LOGS-----------------")               
+                
+                    filter_logs = filter_automl_logs(logs_path)
+                    # print("FILTER LOGs:", filter_logs)
+                    
+                    filter_path = os.path.join(output_directory, 'filter_logs.txt')
+                    
+                    with open(filter_path, mode="w" , encoding="utf-8") as f:
+                        f.write(filter_logs)
+                    
+               
+               
+               
+                
+    # elif sys.argv[1] == 'summarize':
+        
+    #     jobs = iterate_loop()
+        
+    #     with ThreadPoolExecutor(max_workers=0) as executor:
+    #         for logs_path, file_dir, sum_prompt, output_directory, sum_llm, _, _ in jobs:
+                
+    #             print('\t\t\tprompt: ', sum_prompt)
+    #             print( " ")
+    #             print(f"LOGS: {logs_path}")
+                
+    #             # filter_logs = filter_automl_logs(logs_path)
+    #             # # print("FILTER LOGs:", filter_logs)
+                
+    #             # filter_path = os.path.join(output_directory, 'filter_logs')
+                
+    #             # with open(filter_path, mode="w" , encoding="utf-8") as f:
+    #             #     f.write(filter_logs)
+                    
+ 
+    #             args = (logs_path, sum_llm, file_dir,sum_prompt) 
+    #             executor.submit(explain_process, *args)
+  
+    #     print( " " )
+    
+    
     elif sys.argv[1] == 'summarize':
         
         jobs = iterate_loop()
         
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            for logs_path, file_dir, sum_prompt, output_directory, sum_llm, _, _ in jobs:
+        # with ThreadPoolExecutor(max_workers=0) as executor:
+        for logs_path, file_dir, sum_prompt, output_directory, sum_llm, _, _ in jobs:
+            
+            print('\t\t\tprompt: ', sum_prompt)
+            print( " ")
+            print(f"LOGS: {logs_path}")
+            
+            # filter_logs = filter_automl_logs(logs_path)
+            # # print("FILTER LOGs:", filter_logs)
+            
+            # filter_path = os.path.join(output_directory, 'filter_logs')
+            
+            # with open(filter_path, mode="w" , encoding="utf-8") as f:
+            #     f.write(filter_logs)
                 
-                print('\t\t\tprompt: ', sum_prompt)
+
+            explain_process(logs_path, sum_llm, file_dir,sum_prompt)
+
+
+        print( " " )
+ 
+    ### summarization using Hierachical prompt modelling
+    
+    elif sys.argv[1] == 'summarize_Hierachical_prompt':
+        
+        #with ThreadPoolExecutor(max_workers=2) as executor:
+        for task in TASK:
+            for dataset_name, sum_llm in product(dataset_names_for_task[task], LLMs):
+                output_directory = root_dir + '/results_H_new/' + task + '/' + dataset_name + '/' + sum_llm + '/'
+                create_directory(output_directory)
+                summary_dir =  os.path.join(output_directory , 'fact.txt')
+                if(task == "CLASSIFICATION") | (task == "REGRESSION"):
+                    logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'filter_logs.txt')
+                else:
+                    logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'full_log_MainProcess.txt')
+                
                 print( " ")
                 print(f"LOGS: {logs_path}")
- 
-                args = (logs_path, sum_llm, file_dir,sum_prompt) 
-                executor.submit(explain_process, *args)
-  
-        print( " " )
+                
+                fact_extraction(logs_path, sum_llm, summary_dir)
+                
+                
+                
+                
+                # ## aggregate fact
+                # fact_agg_dir =  os.path.join(output_directory , 'fact_agg_dir.json')
+                
+                # # fact_aggregation(summary_dir, sum_llm, fact_agg_dir)
+
+                # args = (logs_path, sum_llm, summary_dir) 
+                # executor.submit(phase_segmentation, *args)
+        
+                # # micro summary generation ##
+                
+
+                # micro_summary_dir = os.path.join(output_directory, 'micro_summary.json')
+                # print( " " )
+                
+                # # import json
+
+                # # with open(summary_dir, "r") as f:
+                # #     phases = json.load(f)
+                    
+                # # micro_summarization(summary_dir, sum_llm, micro_summary_dir, phases) 
+                
+                
+                
+                
+                
+                
+                
+                # # macro summary generation
+                 
+                global_summary_dir = os.path.join(output_directory, 'global_summary_T.txt') 
+                macro_summarization(summary_dir, sum_llm, global_summary_dir, logs_path )  
+        #    
+                ### VERIFICATION STEP
+                verification_dir = os.path.join(output_directory, 'verification.json') 
+                verification(global_summary_dir, logs_path, sum_llm, verification_dir)
+                
+                ###REVISED SUMMARY
+                
+                revised_summary_dir = os.path.join(output_directory, 'global_summary_revised.txt')
+                revised_summary(global_summary_dir, logs_path, verification_dir, sum_llm, revised_summary_dir)
+                    
+            
+    
+    elif sys.argv[1] == 'judge_H': 
+    
+        jobs = iterate_loop()
+        
+        # with ThreadPoolExecutor(max_workers=4) as executor:
+        
+        for task in TASK:
+            for dataset_name, sum_llm in product(dataset_names_for_task[task], LLMs):
+                output_directory = root_dir + '/results_H_new/' + task + '/' + dataset_name + '/' + sum_llm + '/'
+                create_directory(output_directory)
+                summary_dir =  os.path.join(output_directory , 'global_summary_revised.txt')
+                if(task == "CLASSIFICATION") | (task == "REGRESSION"):
+                    logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'filter_logs.txt')
+                else:
+                    logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'full_log_MainProcess.txt')
+    
+                print( " ")
+                print(f"LOGS: {logs_path}")
+
+                for llm_judge, judge_prompt in product(LLMs_judge, JUDGING_PROMPT): 
+                    final_dir = output_directory + judge_prompt + '/'     
+                    judge_dir = os.path.join(output_directory, f'evaluation_'+llm_judge+'.txt')
+                    # judge_dir = os.path.join(output_directory, f'evaluation_{llm_judge}_mismatch.txt')
+                    
+                    print(f"\t\t judging the summary of the logs: {logs_path}")
+                    print(f"\t\t\t using the LLM:", llm_judge, " to judge")
+                    print(f"\t\t Judging the summary located in the directory: {summary_dir}")
+                    print("JUDGING PROMPT:", judge_prompt)  
+                    print(f"judge file directory: {judge_dir}")   
+                    
+                    judging_explanation(logs_path,summary_dir,llm_judge,judge_dir,judge_prompt)
+                    args = (logs_path,summary_dir,llm_judge,judge_dir,judge_prompt)
+                    # executor.submit(judging_explanation, *args)
+                    # gc.collect()                    
+                
+        print( " " )  
+
                 
             
     elif sys.argv[1] == 'judge': 
         
         jobs = iterate_loop()
         
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        # with ThreadPoolExecutor(max_workers=4) as executor:
                 
-            for logs_path, file_dir, sum_prompt, output_directory ,_ ,_, _ in jobs:
-                for llm_judge, judge_prompt in product(LLMs_judge, JUDGING_PROMPT):      
-                    judge_dir = os.path.join(output_directory, f'evaluation_'+llm_judge+'.txt')
-                    
-                    print(f"\t\t judging the summary of the logs:  full_log_MainProcess.txt")
-                    print(f"\t\t\t using the LLM:", llm_judge, " to judge")
-                    print(f"\t\t Judging the summary located in the directory: {file_dir}")
-                    print("JUDGING PROMPT:", judge_prompt)  
-                    print(f"judge file directory: {judge_dir}")   
-                    args = (logs_path,file_dir,llm_judge,judge_dir,judge_prompt)
-                    executor.submit(judging_explanation, *args)
-                    gc.collect()                    
+        for logs_path, file_dir, sum_prompt, output_directory ,_ ,_, _ in jobs:
+            for llm_judge, judge_prompt in product(LLMs_judge, JUDGING_PROMPT):      
+                judge_dir = os.path.join(output_directory, f'evaluation_'+llm_judge+'.txt')
+                # judge_dir = os.path.join(output_directory, f'evaluation_{llm_judge}_mismatch.txt')
+                
+                print(f"\t\t judging the summary of the logs: {logs_path}")
+                print(f"\t\t\t using the LLM:", llm_judge, " to judge")
+                print(f"\t\t Judging the summary located in the directory: {file_dir}")
+                print("JUDGING PROMPT:", judge_prompt)  
+                print(f"judge file directory: {judge_dir}")  
+                judging_explanation(logs_path,file_dir,llm_judge,judge_dir,judge_prompt) 
+                    # args = (logs_path,file_dir,llm_judge,judge_dir,judge_prompt)
+                    # executor.submit(judging_explanation, *args)
+                    # gc.collect()                    
                         
         print( " " )  
             
+            
+     
+     
+     
+     ### summarization using Hierachical prompt modelling MODIFIED METHODS
+    
+    elif sys.argv[1] == 'summarize_H_new':
+        
+        #with ThreadPoolExecutor(max_workers=2) as executor:
+        for task in TASK:
+            for dataset_name, sum_llm in product(dataset_names_for_task[task], LLMs):
+                output_directory = root_dir + '/results_test_H/' + task + '/' + dataset_name + '/' + sum_llm + '/'
+                create_directory(output_directory)
+                summary_dir =  os.path.join(output_directory , 'fact.json')
+                # if(task == "CLASSIFICATION") | (task == "REGRESSION"):
+                #     logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'filter_logs.txt')
+                # else:
+                logs_path = os.path.join(root_dir, 'results', task, dataset_name, 'full_log_MainProcess.txt')
+                
+                print( " ")
+                print(f"LOGS: {logs_path}")
+                
+                phase_segmentation(logs_path, sum_llm, summary_dir)
+                
+                import json
+                with open(summary_dir, "r") as f:
+                    phases = json.load(f)
+                    
+                
+                phase_filter =  os.path.join(output_directory , 'phase_filter.txt')    
+                text = regex_filtering(logs_path, phases)
+                
+                             
+                with open(phase_filter, mode="w" , encoding="utf-8") as f:
+                        f.write(text)
+                    
+                           
+            
+                
+                
+                
+                
+   
+                
+                
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+            
 
     elif sys.argv[1] == 'generate_csv_file':
+        csv_df = [ ]
+        doc = []
         jobs = iterate_loop()
         for logs_path, summary_dir, sum_prompt, output_directory, sum_llm, task, dataset_name in jobs:
             for  llm_judge, judge_prompt in product(LLMs_judge,JUDGING_PROMPT ):
@@ -239,11 +481,16 @@ if __name__ == "__main__":
                 judge_dir = os.path.join(output_directory, f'evaluation_{llm_judge}.txt')           
                 print(f"judge file directory: {judge_dir}")
                 print("JUDGING PROMPT:", judge_prompt)        
-                doc = result_script(judge_dir)
-                print(doc)
-                write_csv(task,dataset_name, sum_llm, sum_prompt, llm_judge, judge_prompt, doc, root_dir+"/"+"result.csv")     
-    
+                row = result_script(judge_dir)
+                # print(f"row: {row}")
+                # doc.append(row)
+                # if len(doc) != 0:
+                #     print(len(doc))
+                #     print(f"list is: {doc}")
+                result_dir = os.path.join(root_dir, f"results_F_H.csv")
+                write_csv(task,dataset_name, sum_llm, sum_prompt, llm_judge, judge_prompt, row , result_dir)     
         
+            
     else:
         '''this is the code to launch an experiment on a particular task, dataset and llm'''
         
@@ -284,5 +531,3 @@ if __name__ == "__main__":
             
             print(" ")
             print('DONE SUMMARIZATION')
-
-
